0b73faaa4f88e2e6ddd56ab387becf9e92205ad8
szdosar <dosar@live.com>
2022-08-26 16:40:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,21 +15,21 @@
 
 # Add a feed source
 # sed -i -E 's/#(src-git.+)(helloworld.+)/\1\2/' feeds.conf.default
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-# sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
-# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # sed -i '$a src-git openappfilter https://github.com/szdosar/OpenAppFilter' feeds.conf.default
-rm feeds.conf.default
-cat > feeds.conf.default <<EOF
-src-git packages https://github.com/coolsnowwolf/packages^d398651
-src-git luci https://github.com/coolsnowwolf/luci^160352f
-src-git routing https://git.openwrt.org/feed/routing.git
-src-git telephony https://git.openwrt.org/feed/telephony.git
-src-git lienol https://github.com/Lienol/openwrt-package
-src-git helloworld https://github.com/fw876/helloworld.git
-src-git kenzo https://github.com/kenzok8/openwrt-packages
-src-git small https://github.com/kenzok8/small
-
-EOF
+# rm feeds.conf.default
+# cat > feeds.conf.default <<EOF
+# src-git packages https://github.com/coolsnowwolf/packages^d398651
+# src-git luci https://github.com/coolsnowwolf/luci^160352f
+# src-git routing https://git.openwrt.org/feed/routing.git
+# src-git telephony https://git.openwrt.org/feed/telephony.git
+# src-git lienol https://github.com/Lienol/openwrt-package
+# src-git helloworld https://github.com/fw876/helloworld.git
+# src-git kenzo https://github.com/kenzok8/openwrt-packages
+# src-git small https://github.com/kenzok8/small
+# 
+# EOF
 
